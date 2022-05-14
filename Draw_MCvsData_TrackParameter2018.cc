640d189ef1ee8d0984900a049a0f07ca0eb273b9
Zero <arashikattun48@gmail.com>
2022-05-14 16:47:14 +0800

    track paramteres done

--- Draw_MCvsData_TrackParameter2018.cc
+++ Draw_MCvsData_TrackParameter2018.cc
@@ -9,7 +9,8 @@ void Draw_MCvsData_TrackParameter2018()
 
   TCanvas    *canvas = new TCanvas("canvas", "", 800, 600);
   TFile      *fin0   = new TFile("RootFile/MonoData2018_Plot.root");
-  fin0->cd();
+	TDirectory *dir0	= (TDirectory*)fin0->Get("HLT_Photon");
+  dir0->cd();
   gStyle->SetOptStat(0);
 
   TDirectory *dir[10];
@@ -61,23 +62,28 @@ void Draw_MCvsData_TrackParameter2018()
     XYPar2_MC[i] = (TH1D *)dir[i]->Get("XYPar2");
   }
 
+	//------------------------------------------------
+	//Draw RZPar0
+
   TH1D *Data_RZPar0;
-  Data_RZPar0 = (TH1D *)fin0->Get("Plot_RZPar0");
-  Data_RZPar0->SetXTitle("cm ( z direction)");
+  Data_RZPar0 = (TH1D *)dir0->Get("RZPar0");
+  Data_RZPar0->SetXTitle("cm (shift in z-direction)");
+  Data_RZPar0->SetYTitle("Entries");
+  Data_RZPar0->SetTitle("");
+  Data_RZPar0->SetMarkerStyle(20);
   Data_RZPar0->Scale(1 / Data_RZPar0->Integral());
 	for(int i=1;i<9;i++){
 					RZPar0_MC[i]->Scale(1/RZPar0_MC[i]->Integral());
 	}
-  Data_RZPar0->SetYTitle("Entries");
-  Data_RZPar0->SetTitle("");
-  Data_RZPar0->Draw("E");
+  Data_RZPar0->Draw("E same");
 	for(int i=1;i<9;i++){
 					RZPar0_MC[i]->Draw("same hist");
 	}
+	Data_RZPar0->SetMaximum(0.07);
 	RZPar0_MC[1]->SetLineColor(2);
 	RZPar0_MC[2]->SetLineColor(30);
 	RZPar0_MC[3]->SetLineColor(4);
-	RZPar0_MC[4]->SetLineColor(6);
+	RZPar0_MC[4]->SetLineColor(12);
 	RZPar0_MC[5]->SetLineColor(38);
 	RZPar0_MC[6]->SetLineColor(8);
 	RZPar0_MC[7]->SetLineColor(9);
@@ -85,99 +91,241 @@ void Draw_MCvsData_TrackParameter2018()
   mgr::SetSinglePad(canvas);
   mgr::SetAxis(Data_RZPar0);
   mgr::DrawEntryRight("2018 13TeV");
+  mgr::DrawCMSLabelOuter(SIMULATION);
+  TLegend *leg = new TLegend(0.20, 0.40, 0.50, 0.70);
+  leg->SetBorderSize(0);
+  leg->SetFillColorAlpha(0, 0);
+  leg->SetTextSize(0.03);
+  leg->AddEntry(RZPar0_MC[1], "1000 GeV", "l");
+  leg->AddEntry(RZPar0_MC[2], "1500 GeV", "l");
+  leg->AddEntry(RZPar0_MC[3], "2000 GeV", "l");
+  leg->AddEntry(RZPar0_MC[4], "2500 GeV", "l");
+  leg->AddEntry(RZPar0_MC[5], "3000 GeV", "l");
+  leg->AddEntry(RZPar0_MC[6], "3500 GeV", "l");
+  leg->AddEntry(RZPar0_MC[7], "4000 GeV", "l");
+  leg->AddEntry(RZPar0_MC[8], "4500 GeV", "l");
+  leg->AddEntry(Data_RZPar0, "data", "p");
+	leg->Draw();
   canvas->SaveAs("./Plots/Data2018RZPar0.pdf");
-/*
+	//------------------------------------------------
+	//Draw RZPar1
   TH1D *Data_RZPar1;
-  Data_RZPar1 = (TH1D *)fin0->Get("RZPar1");
-  Data_RZPar1->Scale(RZPar1_MC->Integral() / Data_RZPar1->Integral());
+  Data_RZPar1 = (TH1D *)dir0->Get("RZPar1");
   Data_RZPar1->SetXTitle("gradient in RZ plane");
   Data_RZPar1->SetYTitle("Entries");
   Data_RZPar1->SetTitle("");
-  Data_RZPar1->Draw("HIST");
+  Data_RZPar1->SetMarkerStyle(20);
+  Data_RZPar1->Scale(1 / Data_RZPar1->Integral());
+	for(int i=1;i<9;i++){
+					RZPar1_MC[i]->Scale(1/RZPar1_MC[i]->Integral());
+	}
+  Data_RZPar1->Draw("E");
+	for(int i=1;i<9;i++){
+					RZPar1_MC[i]->Draw("same hist");
+	}
+	Data_RZPar1->SetMaximum(0.1);
+	RZPar1_MC[1]->SetLineColor(2);
+	RZPar1_MC[2]->SetLineColor(30);
+	RZPar1_MC[3]->SetLineColor(4);
+	RZPar1_MC[4]->SetLineColor(12);
+	RZPar1_MC[5]->SetLineColor(38);
+	RZPar1_MC[6]->SetLineColor(8);
+	RZPar1_MC[7]->SetLineColor(9);
+	RZPar1_MC[8]->SetLineColor(44);
   mgr::SetSinglePad(canvas);
   mgr::SetAxis(Data_RZPar1);
   mgr::DrawEntryRight("2018 13TeV");
+  mgr::DrawCMSLabelOuter(SIMULATION);
+  TLegend *leg2 = new TLegend(0.20, 0.40, 0.50, 0.70);
+  leg2->SetBorderSize(0);
+  leg2->SetFillColorAlpha(0, 0);
+  leg2->SetTextSize(0.03);
+  leg2->AddEntry(RZPar1_MC[1], "1000 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[2], "1500 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[3], "2000 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[4], "2500 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[5], "3000 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[6], "3500 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[7], "4000 GeV", "l");
+  leg2->AddEntry(RZPar1_MC[8], "4500 GeV", "l");
+  leg2->AddEntry(Data_RZPar1, "data", "p");
+	leg2->Draw();
   canvas->SaveAs("./Plots/Data2018RZPar1.pdf");
-
+	//------------------------------------------------
+	//Draw RZPar2
   TH1D *Data_RZPar2;
-  Data_RZPar2 = (TH1D *)fin0->Get("RZcurv");
-  Data_RZPar2->Scale(RZPar2_MC->Integral() / Data_RZPar2->Integral());
+  Data_RZPar2 = (TH1D *)dir0->Get("RZcurv");
   Data_RZPar2->SetXTitle("RZ curvature");
   Data_RZPar2->SetYTitle("Entries");
   Data_RZPar2->SetTitle("");
-  Data_RZPar2->Draw("HIST");
+  Data_RZPar2->SetMarkerStyle(20);
+  Data_RZPar2->Scale(1 / Data_RZPar2->Integral());
+	for(int i=1;i<9;i++){
+					RZPar2_MC[i]->Scale(1/RZPar2_MC[i]->Integral());
+	}
+  Data_RZPar2->Draw("E");
+	for(int i=1;i<9;i++){
+					RZPar2_MC[i]->Draw("same hist");
+	}
+	RZPar2_MC[1]->SetLineColor(2);
+	RZPar2_MC[2]->SetLineColor(30);
+	RZPar2_MC[3]->SetLineColor(4);
+	RZPar2_MC[4]->SetLineColor(12);
+	RZPar2_MC[5]->SetLineColor(38);
+	RZPar2_MC[6]->SetLineColor(8);
+	RZPar2_MC[7]->SetLineColor(9);
+	RZPar2_MC[8]->SetLineColor(44);
   mgr::SetSinglePad(canvas);
   mgr::SetAxis(Data_RZPar2);
   mgr::DrawEntryRight("2018 13TeV");
+  mgr::DrawCMSLabelOuter(SIMULATION);
+  TLegend *leg3 = new TLegend(0.20, 0.40, 0.50, 0.70);
+  leg3->SetBorderSize(0);
+  leg3->SetFillColorAlpha(0, 0);
+  leg3->SetTextSize(0.03);
+  leg3->AddEntry(RZPar2_MC[1], "1000 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[2], "1500 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[3], "2000 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[4], "2500 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[5], "3000 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[6], "3500 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[7], "4000 GeV", "l");
+  leg3->AddEntry(RZPar2_MC[8], "4500 GeV", "l");
+  leg3->AddEntry(Data_RZPar2, "data", "p");
+	leg3->Draw();
   canvas->SaveAs("./Plots/Data2018RZPar2.pdf");
 
+	//------------------------------------------------
+	//Draw XYPar0
   TH1D *Data_XYPar0;
-  Data_XYPar0 = (TH1D *)fin0->Get("XYPar0");
-  Data_XYPar0->Scale(XYPar0_MC->Integral() / Data_XYPar0->Integral());
-  Data_XYPar0->SetXTitle("d_{0} impact parameter");
+  Data_XYPar0 = (TH1D *)dir0->Get("XYPar0");
+  Data_XYPar0->SetXTitle("d_{0} impact parameter (cm)");
   Data_XYPar0->SetYTitle("Entries");
   Data_XYPar0->SetTitle("");
-  Data_XYPar0->Draw("HIST");
+  Data_XYPar0->SetMarkerStyle(20);
+  Data_XYPar0->Scale(1 / Data_XYPar0->Integral());
+	for(int i=1;i<9;i++){
+					XYPar0_MC[i]->Scale(1/XYPar0_MC[i]->Integral());
+	}
+	Data_XYPar0->SetMaximum(0.13);
+  Data_XYPar0->Draw("E");
+	for(int i=1;i<9;i++){
+					XYPar0_MC[i]->Draw("same hist");
+	}
+	XYPar0_MC[1]->SetLineColor(2);
+	XYPar0_MC[2]->SetLineColor(30);
+	XYPar0_MC[3]->SetLineColor(4);
+	XYPar0_MC[4]->SetLineColor(12);
+	XYPar0_MC[5]->SetLineColor(38);
+	XYPar0_MC[6]->SetLineColor(8);
+	XYPar0_MC[7]->SetLineColor(9);
+	XYPar0_MC[8]->SetLineColor(44);
   mgr::SetSinglePad(canvas);
   mgr::SetAxis(Data_XYPar0);
   mgr::DrawEntryRight("2018 13TeV");
+  mgr::DrawCMSLabelOuter(SIMULATION);
+  TLegend *leg4 = new TLegend(0.20, 0.40, 0.50, 0.70);
+  leg4->SetBorderSize(0);
+  leg4->SetFillColorAlpha(0, 0);
+  leg4->SetTextSize(0.03);
+  leg4->AddEntry(XYPar0_MC[1], "1000 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[2], "1500 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[3], "2000 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[4], "2500 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[5], "3000 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[6], "3500 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[7], "4000 GeV", "l");
+  leg4->AddEntry(XYPar0_MC[8], "4500 GeV", "l");
+  leg4->AddEntry(Data_XYPar0, "data", "p");
+	leg4->Draw();
   canvas->SaveAs("./Plots/Data2018XYPar0.pdf");
-
+	//------------------------------------------------
+	//Draw XYPar1
   TH1D *Data_XYPar1;
-  Data_XYPar1 = (TH1D *)fin0->Get("XYPar1");
-  Data_XYPar1->Scale(XYPar1_MC->Integral() / Data_XYPar1->Integral());
+  Data_XYPar1 = (TH1D *)dir0->Get("XYPar1");
   Data_XYPar1->SetXTitle("#phi_{0}");
   Data_XYPar1->SetYTitle("Entries");
   Data_XYPar1->SetTitle("");
-  Data_XYPar1->Draw("HIST");
+  Data_XYPar1->SetMarkerStyle(20);
+  Data_XYPar1->Scale(1 / Data_XYPar1->Integral());
+	for(int i=1;i<9;i++){
+					XYPar1_MC[i]->Scale(1/XYPar1_MC[i]->Integral());
+	}
+  Data_XYPar1->Draw("E");
+	for(int i=1;i<9;i++){
+					XYPar1_MC[i]->Draw("same hist");
+	}
+	XYPar1_MC[1]->SetLineColor(2);
+	XYPar1_MC[2]->SetLineColor(30);
+	XYPar1_MC[3]->SetLineColor(4);
+	XYPar1_MC[4]->SetLineColor(12);
+	XYPar1_MC[5]->SetLineColor(38);
+	XYPar1_MC[6]->SetLineColor(8);
+	XYPar1_MC[7]->SetLineColor(9);
+	XYPar1_MC[8]->SetLineColor(44);
   mgr::SetSinglePad(canvas);
   mgr::SetAxis(Data_XYPar1);
   mgr::DrawEntryRight("2018 13TeV");
+  mgr::DrawCMSLabelOuter(SIMULATION);
+  TLegend *leg5 = new TLegend(0.20, 0.40, 0.50, 0.70);
+  leg5->SetBorderSize(0);
+  leg5->SetFillColorAlpha(0, 0);
+  leg5->SetTextSize(0.03);
+  leg5->AddEntry(XYPar1_MC[1], "1000 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[2], "1500 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[3], "2000 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[4], "2500 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[5], "3000 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[6], "3500 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[7], "4000 GeV", "l");
+  leg5->AddEntry(XYPar1_MC[8], "4500 GeV", "l");
+  leg5->AddEntry(Data_XYPar1, "data", "p");
+	leg5->Draw();
   canvas->SaveAs("./Plots/Data2018XYPar1.pdf");
 
+	//------------------------------------------------
+	//Draw XYPar2
+	canvas->SetLogy();
   TH1D *Data_XYPar2;
-  Data_XYPar2 = (TH1D *)fin0->Get("XYPar2");
-  Data_XYPar2->Scale(XYPar2_MC->Integral() / Data_XYPar2->Integral());
-  Data_XYPar2->SetXTitle("cm (radius)");
+  Data_XYPar2 = (TH1D *)dir0->Get("XYPar2");
+  Data_XYPar2->SetXTitle("radius (m)");
   Data_XYPar2->SetYTitle("Entries");
   Data_XYPar2->SetTitle("");
-  Data_XYPar2->Draw("HIST");
+  Data_XYPar2->SetMarkerStyle(20);
+  Data_XYPar2->Scale(1 / Data_XYPar2->Integral());
+	for(int i=1;i<9;i++){
+					XYPar2_MC[i]->Scale(1/XYPar2_MC[i]->Integral());
+	}
+	Data_XYPar2->SetMaximum(1);
+  Data_XYPar2->Draw("E");
+	for(int i=1;i<9;i++){
+					XYPar2_MC[i]->Draw("same hist");
+	}
+	XYPar2_MC[1]->SetLineColor(2);
+	XYPar2_MC[2]->SetLineColor(30);
+	XYPar2_MC[3]->SetLineColor(4);
+	XYPar2_MC[4]->SetLineColor(12);
+	XYPar2_MC[5]->SetLineColor(38);
+	XYPar2_MC[6]->SetLineColor(8);
+	XYPar2_MC[7]->SetLineColor(9);
+	XYPar2_MC[8]->SetLineColor(44);
   mgr::SetSinglePad(canvas);
   mgr::SetAxis(Data_XYPar2);
   mgr::DrawEntryRight("2018 13TeV");
+  mgr::DrawCMSLabelOuter(SIMULATION);
+  TLegend *leg6 = new TLegend(0.20, 0.40, 0.50, 0.70);
+  leg6->SetBorderSize(0);
+  leg6->SetFillColorAlpha(0, 0);
+  leg6->SetTextSize(0.03);
+  leg6->AddEntry(XYPar2_MC[1], "1000 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[2], "1500 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[3], "2000 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[4], "2500 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[5], "3000 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[6], "3500 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[7], "4000 GeV", "l");
+  leg6->AddEntry(XYPar2_MC[8], "4500 GeV", "l");
+  leg6->AddEntry(Data_XYPar2, "data", "p");
+	leg6->Draw();
   canvas->SaveAs("./Plots/Data2018XYPar2.pdf");
-*/
-  //        double dEdXSigCut_ = 9;
-  //        double f51Cut_ = 0.85;
-  //	TH2D* ABCD;
-  //	ABCD = (TH2D*)fin0->Get("Flow_HLT_Energy_ABCD");
-  //        ABCD->SetXTitle("f51");
-  //        ABCD->SetYTitle("dE/dX significance");
-  //        ABCD->SetTitle("");
-  //        ABCD->SetMarkerStyle(7);
-  //        ABCD->Draw("COLZ");
-  //        mgr::SetSinglePad(canvas);
-  //        mgr::SetAxis(ABCD);
-  //        mgr::DrawCMSLabelOuter(SIMULATION);
-  //        mgr::DrawEntryRight("2018 13TeV Mass 1000GeV 16389 Entries");
-  //        TLine *TightY = new TLine(0,dEdXSigCut_,1.1,dEdXSigCut_);
-  //        TightY->SetLineColor(4);
-  //        TightY->SetLineStyle(7);
-  //        TightY->SetLineWidth(3);
-  //        TightY->Draw("same");
-  //        TLine *TightX = new TLine(f51Cut_,0,f51Cut_,30);
-  //        TightX->SetLineColor(4);
-  //        TightX->SetLineStyle(7);
-  //        TightX->SetLineWidth(3);
-  //        TightX->Draw("same");
-  //        TLine *LooseY = new TLine(0,7,1.1,7);
-  //        LooseY->SetLineColor(38);
-  //        LooseY->SetLineStyle(7);
-  //        LooseY->Draw("same");
-  //        TLine *LooseX = new TLine(0.6,0,0.6,30);
-  //        LooseX->SetLineColor(38);
-  //        LooseX->SetLineStyle(7);
-  //        LooseX->Draw("same");
-  //	canvas->SaveAs("ABCD(Ecut+HLT_Photon200).pdf");
-  //
 }
